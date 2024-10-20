8174243c516a7080dfa799e56aee2cf00959e3ba
Joshua Reyes <Joshua.Reyes.2023@live.rhul.ac.uk>
2024-10-20 13:52:28 +0100

    added Module Class

---
+++ CW1/src/main/java/uk/ac/rhul/cs2800/Module.java
@@ -0,0 +1,33 @@
+package uk.ac.rhul.cs2800;
+
+public class Module {
+  private String code;
+  private String name;
+  private boolean mnc;
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public boolean getMnc() {
+    return mnc;
+  }
+
+  public void setMnc(boolean mnc) {
+    this.mnc = mnc;
+  }
+
+
+}
