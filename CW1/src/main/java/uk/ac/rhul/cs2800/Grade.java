f6107794e73318d27b938dc7e834196a3e766179
Joshua Reyes <Joshua.Reyes.2023@live.rhul.ac.uk>
2024-10-20 14:38:16 +0100

    added Grade Class

---
+++ CW1/src/main/java/uk/ac/rhul/cs2800/Grade.java
@@ -0,0 +1,18 @@
+package uk.ac.rhul.cs2800;
+
+/**
+ * this is the class for representing Grades for Modules.
+ */
+public class Grade {
+  private Integer score;
+
+  public Integer getScore() {
+    return score;
+  }
+
+  public void setScore(Integer score) {
+    this.score = score;
+  }
+
+
+}
