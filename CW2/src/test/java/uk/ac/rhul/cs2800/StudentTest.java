10db20188c73e6b070d8ae3e7ef193469d411386
Joshua Reyes <Joshua.Reyes.2023@live.rhul.ac.uk>
2024-11-10 15:22:57 +0000

    Started Refactoring Tests

--- CW2/src/test/java/uk/ac/rhul/cs2800/StudentTest.java
+++ CW2/src/test/java/uk/ac/rhul/cs2800/StudentTest.java
@@ -3,6 +3,7 @@ package uk.ac.rhul.cs2800;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uk.ac.rhul.cs2800.exception.NoGradeAvailableException;
 import uk.ac.rhul.cs2800.exception.NoRegistrationException;
@@ -17,24 +18,25 @@ import uk.ac.rhul.cs2800.model.Student;
  */
 public class StudentTest {
 
+  Student student;
+
+  @BeforeEach
+  void beforeEach() {
+    Student student = new Student();
+    student.setId(1);
+    student.setFirstName("alice");
+    student.setLastName("brandy");
+    student.setUsername("BossBrandy");
+    student.setEmail("alicebrandy@gmail.com");
+  }
+
   @Test
   public void getandsetStudentFields() {
     // test 1
-    Student student = new Student();
-
-    student.setId(1);
     final long id = student.getId();
-
-    student.setFirstName("alice");
     final String name = student.getFirstName();
-
-    student.setLastName("brandy");
-    final String lastname = student.getLastName();
-
-    student.setUsername("BossBrandy");
+    final String lastname = student.getLastName();;
     final String username = student.getUsername();
-
-    student.setEmail("alicebrandy@gmail.com");
     final String email = student.getEmail();
 
     assertEquals(1, id);
