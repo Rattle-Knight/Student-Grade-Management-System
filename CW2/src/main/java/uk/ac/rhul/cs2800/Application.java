41dc3d90af77664e2483d6fedeb179a1a8cabe9f
Joshua Reyes <Joshua.Reyes.2023@live.rhul.ac.uk>
2024-11-11 14:08:50 +0000

    #4 added Application.java

---
+++ CW2/src/main/java/uk/ac/rhul/cs2800/Application.java
@@ -0,0 +1,17 @@
+package uk.ac.rhul.cs2800;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * This is the main SpringBoot Application.
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
