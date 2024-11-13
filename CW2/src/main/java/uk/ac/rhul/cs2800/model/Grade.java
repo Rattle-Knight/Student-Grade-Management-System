6f89c562fa8793f8b4886d7665d8eb1736d2221d
Joshua Reyes <Joshua.Reyes.2023@live.rhul.ac.uk>
2024-11-13 14:10:39 +0000

    #4 mapped grade values values

--- CW2/src/main/java/uk/ac/rhul/cs2800/model/Grade.java
+++ CW2/src/main/java/uk/ac/rhul/cs2800/model/Grade.java
@@ -1,9 +1,25 @@
 package uk.ac.rhul.cs2800.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 /**
  * this is the class for representing Grades for Modules.
  */
+@Entity
 public class Grade {
+
+  @Id
+  @GeneratedValue
+  long id;
+
+  @ManyToOne
+  @JoinColumn(name = "student_id")
+  Student student;
+
   private Integer score;
   private String moduleReference;
 
