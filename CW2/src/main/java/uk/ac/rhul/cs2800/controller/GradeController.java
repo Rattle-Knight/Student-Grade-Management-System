cfa4b9c242321073448a5e63e9393fcfd3ddca50
Joshua Reyes <Joshua.Reyes.2023@live.rhul.ac.uk>
2024-12-05 13:46:39 +0000

    #9 added javadoc for grade Controller

--- CW2/src/main/java/uk/ac/rhul/cs2800/controller/GradeController.java
+++ CW2/src/main/java/uk/ac/rhul/cs2800/controller/GradeController.java
@@ -15,10 +15,22 @@ import uk.ac.rhul.cs2800.repository.GradeRepository;
 public class GradeController {
   private final GradeRepository gradeRepository;
 
+  /**
+   * this is the public constructor for GradeController, the gradeRepository object (bean) is
+   * initialised by Spring and injected to the GradeController.
+   *
+   * @param gradeRepository initialised by spring for the dependency injection.
+   */
   public GradeController(GradeRepository gradeRepository) {
     this.gradeRepository = gradeRepository;
   }
 
+  /**
+   * this method responds to the POST command and maps the values onto a Grade Object.
+   *
+   * @param params this is bound to the body of the web request and will take in values
+   * @return the saved grade object
+   */
   @PostMapping(value = "/grades/addGrade")
   public ResponseEntity<Grade> addGrade(@RequestBody Map<String, String> params) {
     // Find the student by using student_id
