6058f548fd760bacfa259ca99b2cf85dd7a90df3
Joshua Reyes <Joshua.Reyes.2023@live.rhul.ac.uk>
2024-12-05 19:51:23 +0000

    #9 implemented gradeControllerTest

--- CW2/src/test/java/uk/ac/rhul/cs2800/controller/GradeControllerTest.java
+++ CW2/src/test/java/uk/ac/rhul/cs2800/controller/GradeControllerTest.java
@@ -1,11 +1,23 @@
 package uk.ac.rhul.cs2800.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import uk.ac.rhul.cs2800.model.Grade;
 import uk.ac.rhul.cs2800.model.Module;
 import uk.ac.rhul.cs2800.model.Student;
 import uk.ac.rhul.cs2800.repository.GradeRepository;
@@ -64,6 +76,32 @@ public class GradeControllerTest {
 
   }
   
+  @Test
+  public void addGradeTest() throws JsonProcessingException, Exception {
+    Map<String, String> params = new HashMap<String, String>();
+    params.put("student_id", "0");;
+    params.put("module_code", "CS2800");
+    params.put("score", "90");
+    
+    MvcResult action = mockMvc
+        .perform(MockMvcRequestBuilders.post("/grades/addGrade")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(params)).accept(MediaType.APPLICATION_JSON))
+        .andReturn();
+    assertEquals(HttpStatus.OK.value(), action.getResponse().getStatus());
 
+    Grade grade = objectMapper.readValue(action.getResponse().getContentAsString(), Grade.class);
+    
+    //tests ids of student
+    assertEquals(grade.getStudent().getId(), student.getId());
+    
+    //tests ids of module
+    assertEquals(grade.getModule().getCode(), module.getCode());
+    
+    // tests score
+    assertEquals(grade.getScore(), 90);
+
+
+  }
   
 }
